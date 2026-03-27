dfcbb37fa9fdf80f2a7b2cedc230ed8b27a6e9e5
Artem Levchenko <levchencode@gmail.com>
2026-03-28 01:24:16 +0200

    chore(comments): shared comment types and schemas

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./auth.schema.js";
 export * from "./category.schema.js";
+export * from "./comment.schema.js";
 export * from "./recipe.schema.js";
 export * from "./types.js";
 export * from "./utils.js";
