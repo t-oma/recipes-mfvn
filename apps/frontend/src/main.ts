0041b29a466491c244d194afff6e20ea5c653d45
Artem Levchenko <levchencode@gmail.com>
2026-04-14 22:46:33 +0300

    chore: install and configure PrimeVue

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,13 +1,22 @@
 import "./assets/main.css";
 
+import Aura from "@primeuix/themes/aura";
 import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
 import { createApp } from "vue";
-
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: "html.dark",
+    },
+  },
+});
 app.use(createPinia());
 app.use(router);
 
