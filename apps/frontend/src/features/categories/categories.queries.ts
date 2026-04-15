bb5d875f072a914946458a637383ce4b42b85765
Artem Levchenko <levchencode@gmail.com>
2026-04-15 13:41:01 +0300

    chore(category): api & queries

---
+++ apps/frontend/src/features/categories/categories.queries.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/vue-query";
+import { getCategories } from "./categories.api";
+
+const categoryKeys = {
+  all: ["categories"] as const,
+};
+
+export function useCategories() {
+  return useQuery({
+    queryKey: categoryKeys.all,
+    queryFn: getCategories,
+    staleTime: 5 * 60 * 1000,
+  });
+}
