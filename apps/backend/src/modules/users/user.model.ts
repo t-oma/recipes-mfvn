a2c5ba8263b0e1fe11d82c2e7d11f2922f34226d
Artem Levchenko <levchencode@gmail.com>
2026-04-30 22:13:07 +0300

    refactor(user): remove password hashing & comparing logic to dedicated service

--- apps/backend/src/modules/users/user.model.ts
+++ apps/backend/src/modules/users/user.model.ts
@@ -1,16 +1,13 @@
 import type { UserRole } from "@recipes/shared";
-import bcrypt from "bcryptjs";
 import type { Model } from "mongoose";
 import { model, Schema } from "mongoose";
 import type { BaseDocument } from "@/common/types/mongoose.js";
-import { env } from "@/config/env.js";
 
 export interface UserDocument extends BaseDocument {
   email: string;
   password: string;
   name: string;
   role: UserRole;
-  comparePassword(candidate: string): Promise<boolean>;
 }
 
 export interface UserModelType extends Model<UserDocument> {}
@@ -36,17 +33,6 @@ const userSchema = new Schema<UserDocument, UserModelType>(
     timestamps: true,
   },
 );
-
-userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
-  this.password = await bcrypt.hash(this.password, env.BCRYPT_SALT_ROUNDS);
-});
-
-userSchema.methods.comparePassword = async function (
-  candidate: string,
-): Promise<boolean> {
-  return bcrypt.compare(candidate, this.password);
-};
 
 export const USER_MODEL_NAME = "User";
 export const UserModel = model<UserDocument, UserModelType>(
