567439757a8481df804e73997a59b11974fd2ff8
Artem Levchenko <levchencode@gmail.com>
2026-05-20 21:49:58 +0300

    refactor(user): move user to entities

--- apps/frontend/src/entities/user/api/user.api.ts
+++ apps/frontend/src/entities/user/api/user.api.ts
@@ -1,5 +1,30 @@
-import type { CommentDetails, CommentQuery, Paginated } from "@recipes/shared";
+import type {
+  CommentDetails,
+  CommentQuery,
+  FavoriteQuery,
+  Paginated,
+  RecipeListItem,
+} from "@recipes/shared";
 import { apiClient } from "@/shared/api/client";
+
+/**
+ * @todo Implement retriving favorite recipes for the user other than the current one.
+ *
+ * Get recipes favorited by the current user.
+ *
+ * @param user - user id. NOTE: This paramater is ignored for now.
+ * @param query.page - page number.
+ * @param query.limit - number of items per page.
+ * @returns Paginated list of favorite recipes.
+ */
+export function getUserFavorites(
+  _user: string,
+  { page = 1, limit = 20 }: Partial<FavoriteQuery>,
+) {
+  return apiClient<Paginated<RecipeListItem>>("/api/users/me/favorites", {
+    query: { page, limit },
+  });
+}
 
 /**
  * @todo Implement retriving comments for the user other than the current one.
--- apps/frontend/src/entities/user/api/user.queries.ts
+++ apps/frontend/src/entities/user/api/user.queries.ts
@@ -1,8 +1,8 @@
-import type { CommentQuery } from "@recipes/shared";
+import type { CommentQuery, FavoriteQuery } from "@recipes/shared";
 import { queryOptions } from "@tanstack/vue-query";
 import type { MaybeRef } from "vue";
 import { toValue } from "vue";
-import { getUserComments } from "./user.api";
+import { getUserComments, getUserFavorites } from "./user.api";
 
 const userKeys = {
   all: ["users"] as const,
@@ -10,7 +10,19 @@ const userKeys = {
   details: (user: string) => [...userKeys.all, user] as const,
   comments: (user: string, filters: Partial<CommentQuery>) =>
     [...userKeys.details(user), "comments", filters] as const,
+  favorites: (user: string, filters: Partial<FavoriteQuery>) =>
+    [...userKeys.details(user), "favorites", filters] as const,
 };
+
+export function userFavoriteRecipeListOptions(
+  user: string = "me",
+  filters: MaybeRef<Partial<FavoriteQuery>>,
+) {
+  return queryOptions({
+    queryKey: userKeys.favorites(user, toValue(filters)),
+    queryFn: () => getUserFavorites(user, toValue(filters)),
+  });
+}
 
 export function userCommentListOptions(
   user: string = "me",
