1d808d4a1a6d93b8d07e013211d39509908615ba
Artem Levchenko <levchencode@gmail.com>
2026-05-22 19:40:34 +0300

    chore(styles): primary colors & primevue conf

--- apps/frontend/src/app/main.ts
+++ apps/frontend/src/app/main.ts
@@ -21,9 +21,24 @@ const preset = definePreset(Aura, {
       md: "0.375rem",
       lg: "0.5rem",
       xl: "0.75rem",
+      "2xl": "1rem",
     },
   },
   semantic: {
+    primary: {
+      0: "#ffffff",
+      50: "var(--color-terracotta-50)",
+      100: "var(--color-terracotta-100)",
+      200: "var(--color-terracotta-200)",
+      300: "var(--color-terracotta-300)",
+      400: "var(--color-terracotta-400)",
+      500: "var(--color-terracotta-500)",
+      600: "var(--color-terracotta-600)",
+      700: "var(--color-terracotta-700)",
+      800: "var(--color-terracotta-800)",
+      900: "var(--color-terracotta-900)",
+      950: "var(--color-terracotta-950)",
+    },
     formField: {
       borderRadius: "{borderRadius.xl}",
       paddingY: "0.50rem",
@@ -31,16 +46,34 @@ const preset = definePreset(Aura, {
         width: "2px",
         style: "solid",
         color:
-          "color-mix(in oklab, var(--color-terracotta) /* #c27a54 */ 20%, transparent);",
+          "color-mix(in oklab, var(--color-terracotta) /* #c27a54 */ 20%, transparent)",
       },
     },
     colorScheme: {
       light: {
+        primary: {
+          color: "var(--color-terracotta-500)",
+          hoverColor: "var(--color-terracotta-600)",
+          activeColor: "var(--color-terracotta-700)",
+        },
         formField: {
           background: "#ffffff",
           borderColor: "{stone.200}",
           hoverBorderColor: "var(--color-terracotta-light)",
           focusBorderColor: "var(--color-terracotta)",
+        },
+      },
+    },
+  },
+  components: {
+    button: {
+      root: {
+        borderRadius: "{borderRadius.2xl}",
+        paddingX: "1rem",
+        paddingY: "0.75rem",
+        sm: {
+          paddingX: "0.75rem",
+          paddingY: "0.5rem",
         },
       },
     },
